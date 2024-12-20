a35c97a79f0943050747acac8fd362884325ea3e
Fikret Erkara <fikreterkara_26@hotmail.com>
2024-12-21 00:53:08 +0300

    added shadcn ui components

---
+++ app/components/QrCodeGenerator.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const QrCodeGenerator = () => {
+  return (
+    <div className="relative z-10 mx-6 flex max-w-[1250px] w-full min-h-[700px] h-full">
+      <div className="flex-1 flex flex-col w-full h-auto mx-auto bg-[#ecf7ff]/80 backdrop-blur-md shadow-sm border-2 border-white/40 rounded-xl"></div>
+    </div>
+  );
+};
+
+export default QrCodeGenerator;
